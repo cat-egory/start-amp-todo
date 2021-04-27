ec69f146d1a4fa0240961cadbb42b2a64ab1b602
cat-egory <xtheblackeyes@gmail.com>
2021-04-27 22:34:48 +0900

    feat: 첫 graphQL 연동

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import {createApp} from 'vue';
+import App from './App.vue';
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
 import {
-    applyPolyfills,
-    defineCustomElements,
+  applyPolyfills,
+  defineCustomElements,
 } from '@aws-amplify/ui-components/loader';
 
 Amplify.configure(aws_exports);
 applyPolyfills().then(() => {
-    defineCustomElements(window);
+  defineCustomElements(window);
 });
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
